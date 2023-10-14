36dbe26484c70e6a634bada4930cfa26dd97624c
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2023-10-14 04:26:47 +0200

    Feat: Update clean.py

--- clean.py
+++ clean.py
@@ -1,1 +1,9 @@
 
+# Simon is a good friend that help Stian learn Git.
+
+# Sverre will help Stian learn about the good world of the clean lord
+
+
+def clean() -> None:
+  print("Clean code is a journey")
+  print("That starts and never ends.")
